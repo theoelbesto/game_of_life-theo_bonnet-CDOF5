f8234183c28ce8f73b914d67ff566ddbac8e68e5
theoelbesto <148050593+theoelbesto@users.noreply.github.com>
2025-01-14 14:41:51 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='https://github.com/theoelbesto/game_of_life-theo_bonnet-CDOF5',
+ version='1.0',
+ author='theoelbesto',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
